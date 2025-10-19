7f27b9493e91f72709dd019c4a6430dfb07a05a5
Selvaguru <49345010+selvaguru-s@users.noreply.github.com>
2025-10-19 18:59:50 +0530

    Remove Firebase config from firebase.js
    Removed sensitive Firebase configuration details.

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import { getAuth } from 'firebase/auth'
 
 // Replace with your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyDxHgXvAxNhkj1IPCoW9uKrLZeIUz-nyx8",
-  authDomain: "selvaguru-e613b.firebaseapp.com",
-  projectId: "selvaguru-e613b",
-  storageBucket: "selvaguru-e613b.firebasestorage.app",
-  messagingSenderId: "914201586585",
-  appId: "1:914201586585:web:c1a2685c3d259e7167c108",
-  measurementId: "G-0X0F50JRT8"
+
 }
 
 // Initialize Firebase
